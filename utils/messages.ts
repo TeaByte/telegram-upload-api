d55f342b3ccbe8e0fe638fcc6a7d6ac101d49f1c
teabyte <yazan4code@gmail.com>
2024-05-08 23:34:24 +0300

    Fixing missed return

--- utils/messages.ts
+++ utils/messages.ts
@@ -1,7 +1,7 @@
 import { Context } from "https://deno.land/x/hono@v4.0.8/mod.ts";
 
 export function errorResponse(c: Context, errorMessage: string) {
-  c.json(
+  return c.json(
     {
       error: errorMessage,
     },
